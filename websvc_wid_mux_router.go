2ac5a40b8f14a1e82fa825470290329302c66768
sumitramteke <sumitvramteke@gmail.com>
2016-04-23 07:46:03 +0530

    basic routers

--- websvc_wid_mux_router.go
+++ websvc_wid_mux_router.go
@@ -10,13 +10,13 @@ import (
 )
 
 
-func main() {
+func main2() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", Index)
+	router.HandleFunc("/", Index2)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 
-func Index(w http.ResponseWriter, r *http.Request) {
+func Index2(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
